bb5b73ce66c29bfe59cfbf9153c9f6cbdbacb043
Dan Reuman <reuman@ku.edu>
2019-02-08 15:34:57 -0600

    some changes to early stage data cleaning script

--- Mumps_MMueller.R
+++ Mumps_MMueller.R
@@ -1,30 +1,34 @@
-d<-Mumps
+#import the data
+d<-read.csv(file="./Data/Mumps.csv",stringsAsFactors=FALSE) #always use read.csv or read.table or some similar command to bring data in
 dim(d)
 names(d)
 class(d)
 
-unique(d$V3) #all Mumps
-
-unique(d$V14) #some cummulative, some noncummulative
-
-unique(d$V6) #all USA
-unique(d$V7)
-
-locs<-unique(d[,c("V6","V7")])
+#check some simple tthings
+unique(d$PathogenName) #all Mumps
+unique(d$PartOfCumulativeCountSeries) #some cummulative, some noncummulative
+unique(d$CountryName) #all USA
+unique(d$CountryISO)
+locs<-unique(d[,c("CountryName","CountryISO")])
 locs
+locs2<-unique(d[,c("Admin1Name","Admin1ISO")])
+locs2
+locs2<-locs2[order(locs2$Admin1Name,locs2$Admin1ISO),]
+locs2 #All 50 states plus American Samoa, District of COlumbia, Guam, 
+#Northern Mariana Islands, Puerto Rico, and Virgin Islands
+dim(locs2)
 
-locs2<-unique(d[,c("V8","V9")])
-locs2<-locs2[order(locs2$V8,locs2$V9),]
-locs2 #All 50 states plus American Samoa, District of COlumbia, Guam, Northern Mariana Islands, Puerto Rico, and Virgin Islands
+head(d[d$PartOfCumulativeCountSeries==1 & d$Admin1Name=="KANSAS",],5)
+tail(d[d$PartOfCumulativeCountSeries==0 & d$Admin1Name=="KANSAS",],5)
 
-head(d[d$V14==1 & d$V8=="KANSAS",],100)
-tail(d[d$V14==0 & d$V8=="KANSAS",],20)
+unique(d$AgeRange) #kind of pointless, just 0-130, not helpful when it comes to comparisons with vaccination frequency which is split by age...
+unique(d$PlaceOfAcquisition) #Place of Aquisition - only NA
+unique(d$DiagnosisCertainty) #Diagnosis Certainty - only NA
+unique(d$SourceName) #Source Name - Only US Nationally Notifiable Disease Surveillance System
+unique(d$Fatalities) #Fatalities - none
+unique(d$PathogenName) #only mumps
+unique(d$PathogenTaxonID) #Pathogen Taxon ID - only 11161
 
-unique(d$V15) #kind of pointless, just 0-130, not helpful when it comes to comparisons with vaccination frequency which is split by age...
-unique(d$V17) #Place of Aquisition - only NA
-unique(d$V18) #Diagnosis Certainty - only NA
-unique(d$V19) #Source Name - Only US Nationally Notifiable Disease Surveillance System
-unique(d$V5) #Fatalities - none
-unique(d$V4) #Pathogen Taxon ID - only 11161
-
-#Trying to rename columns so they are more descriptive than just numbers
+#keep only the non-cummulative?
+d<-d[d$PartOfCumulativeCountSeries==0,]
+dim(d)
